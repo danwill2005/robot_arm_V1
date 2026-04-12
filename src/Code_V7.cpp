66f65a5e7ded787ba17d55d0a3671ac8f156acde
danwill2005 <dawilliams2005@icloud.com>
2026-04-12 21:26:24 +0100

    Atomised functions
    This code atomises a lot of the functions in previous iterations, for example moving to 180, 0, 90 degrees are all their own function and have pointers passed to them for the respective servo, then the startup function contains these three functions. The simultaneous movement function also has a helper function now that takes a target and servo as input. The next step is to code a python script for a GUI that allows you to adjust the angle of each servo.

---
+++ src/Code_V7.cpp
@@ -0,0 +1,298 @@
+/*
+ * robot_arm.cpp
+ *
+ * Created: 09/04/2026
+ * Author : Daniel Williams
+ */ 
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <util/atomic.h>
+
+
+//This function sets up the registers in Timer 1 (16 bit) for Normal port and counter operation
+void setup_pwm(){
+	
+	//TCCR1A register set to Normal port and clock operation
+	TCCR1A = 0x00;
+	
+	//sets prescalar to 8
+	TCCR1B = (1<<CS11);
+	
+	//Enables interrupt for TIMER1_COMPA
+	TIMSK1 = (1<<OCIE1A); 
+	
+	//initialise OCR1A
+	OCR1A = 0;
+	
+	//Enable global interrupts
+	sei();
+}
+
+
+void setup_ports(){
+	//sets pins 5,6,7 to output
+	DDRD |= (1<<DDD5) | (1<<DDD6) | (1<<DDD7);
+	
+	//sets pins 8,9,9 to output
+	DDRB |= (1<<DDB0) | (1<<DDB1) | (1<<DDB2);
+	
+}
+
+//cycle stage for the switch case, allows for a series of pulses within the 20ms window
+volatile uint8_t cycle_stage = 0;
+
+//these will be the tick values that equate to the ms value of the pulse width that determines a specific angle, servos have range 0.5-2.5ms --> 1000 - 5000 ticks
+volatile uint16_t servo1_angle = 3000;
+volatile uint16_t servo2_angle = 3000;
+volatile uint16_t servo3_angle = 3000;
+volatile uint16_t servo4_angle = 3000;
+volatile uint16_t servo5_angle = 3000;
+volatile uint16_t servo6_angle = 3000;
+
+ISR(TIMER1_COMPA_vect){
+	
+	switch(cycle_stage){
+		//start of 20ms cycle
+		//want to set the pulse of servo 1
+		case 0:
+			PORTD |= (1<<PORTD5);//sets pin 5 to HIGH
+			OCR1A += servo1_angle; // sets the next interrupt to be the end of servo 1 pulse width
+			cycle_stage = 1;
+		break;
+		//turn off servo1 and wait until the end of the 2.5ms slot
+		case 1:
+			PORTD &= ~(1<<PORTD5);//pin 5 LOW
+			OCR1A += (5000-servo1_angle);// gives servo 1 a 2.5ms slot
+			cycle_stage = 2;
+		break;
+		//turn on servo 2
+		case 2:
+			PORTD |= (1<<PORTD6);//pin 6 HIGH
+			OCR1A += servo2_angle; //sets the next interrupt to be the end of servo 2 pulse width
+			cycle_stage = 3;			
+		break;
+		//turn off servo2 pulse and wait until end of 2.5ms slot
+		case 3:
+			PORTD &= ~(1<<PORTD6);//pin 6 LOW
+			OCR1A += (5000-servo2_angle);// gives servo 2 a 2.5ms slot
+			cycle_stage = 4;	
+		break;
+		//turn on servo 3
+		case 4:
+			PORTD |= (1<<PORTD7);//pin 7 HIGH
+			OCR1A += servo3_angle; //sets the next interrupt to be the end of servo 3 pulse width
+			cycle_stage = 5;
+		break;		
+		//turn off servo3 pulse and wait until end of 2.5ms slot
+		case 5:
+			PORTD &= ~(1<<PORTD7);//pin 7 LOW
+			OCR1A += (5000-servo3_angle);// gives servo 3 a 2.5ms slot
+			cycle_stage = 6;
+		break;
+		//turn on servo 4
+		case 6:
+			PORTB |= (1<<PORTB0);//pin 8 HIGH
+			OCR1A += servo4_angle; //sets the next interrupt to be the end of servo 4 pulse width
+			cycle_stage = 7;
+		break;
+		//turn off servo4 pulse and wait until end of 2.5ms slot
+		case 7:
+			PORTB &= ~(1<<PORTB0);//pin 8 LOW
+			OCR1A += (5000-servo4_angle);// gives servo 4 a 2.5ms slot
+			cycle_stage = 8;
+		break;
+		//turn on servo 5
+		case 8:
+			PORTB |= (1<<PORTB1);//pin 9 HIGH
+			OCR1A += servo5_angle; //sets the next interrupt to be the end of servo 5 pulse width
+			cycle_stage = 9;
+		break;
+		//turn off servo5 pulse and wait until end of 2.5ms slot
+		case 9:
+			PORTB &= ~(1<<PORTB1);//pin 9 LOW
+			OCR1A += (5000-servo5_angle);// gives servo 5 a 2.5ms slot
+			cycle_stage = 10;
+		break;
+		//turn on servo 6
+		case 10:
+			PORTB |= (1<<PORTB2);//pin 10 HIGH
+			OCR1A += servo6_angle; //sets the next interrupt to be the end of servo 6 pulse width
+			cycle_stage = 11;
+		break;
+		//turn off servo6 and wait until the end off 20ms cycle
+		case 11:
+			PORTB &= ~(1<<PORTB2);//pin 10 LOW
+			
+			//40000 ticks = 20ms, 16MHz/8 = 2MHz, 0.02/(1/2000000) = 40000;
+			OCR1A += (40000-30000);//30000 is the sum of 6*5000 tick slots for the servos
+			cycle_stage = 0;
+			break;
+	}
+}
+
+void sweep_to_180_degrees(volatile uint16_t *servo_x_angle){
+	uint16_t current_value;
+	
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		current_value = *servo_x_angle;//prevents interrupt during reading of the while loops in the function
+	}
+	
+	while(current_value < 5000){ //rotates to 180 degrees
+			ATOMIC_BLOCK(ATOMIC_RESTORESTATE){//prevents interrupt during reading 16 bit integer
+				*servo_x_angle+=75;// =~ 3degree steps
+				current_value = *servo_x_angle;
+			}
+			_delay_ms(20); //allows for slower movement
+	}	
+	
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		*servo_x_angle = 5000;
+	}
+}
+
+void sweep_to_0_degrees(volatile uint16_t *servo_x_angle){
+	
+	uint16_t current_value;
+	
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		current_value = *servo_x_angle;//prevents interrupt during reading of the while loops in the function
+	}
+	
+	while(current_value > 1000){ //rotates to 0 degrees
+			ATOMIC_BLOCK(ATOMIC_RESTORESTATE){//prevents interrupt during reading 16 bit integer
+				*servo_x_angle-=75;// =~ 3degree steps
+				current_value = *servo_x_angle;
+			}
+			_delay_ms(20); //allows for slower movement
+	}
+	
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		*servo_x_angle = 1000;
+	}
+}
+
+void sweep_to_90_degrees(volatile uint16_t *servo_x_angle){
+	
+	uint16_t current_value;
+	
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		current_value = *servo_x_angle;//prevents interrupt during reading of the while loops in the function
+	}
+	
+	while((current_value > 3075) || (current_value < 2925)){ //rotates to 180 degrees
+		if(current_value < 2925){
+			ATOMIC_BLOCK(ATOMIC_RESTORESTATE){//prevents interrupt during reading 16 bit integer
+				*servo_x_angle+=75;// =~ 3degree steps
+				current_value = *servo_x_angle;
+			}
+			_delay_ms(20); //allows for slower movement
+		}else if(current_value > 3075){
+			ATOMIC_BLOCK(ATOMIC_RESTORESTATE){//prevents interrupt during reading 16 bit integer
+				*servo_x_angle-=75;// =~ 3degree steps
+				current_value = *servo_x_angle;
+			}		
+			_delay_ms(20);	
+		}
+	}
+	
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		*servo_x_angle = 3000;
+	}
+
+}
+
+void servo_x_startup(volatile uint16_t *servo_x_angle){
+	//startup function for each servo
+	//goes to 180 deg, then to 0, then to 90
+	
+	sweep_to_180_degrees(servo_x_angle);
+			
+	_delay_ms(1000);
+			
+	sweep_to_0_degrees(servo_x_angle);
+			
+	_delay_ms(1000);
+			
+	sweep_to_90_degrees(servo_x_angle);
+	
+}
+
+uint8_t move_by_step_until_target(volatile uint16_t *servo_x_angle, uint16_t target){
+	
+	uint16_t current_value;
+	
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		current_value = *servo_x_angle;
+	}
+	
+	if(current_value < (target - 75)){
+		ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+			*servo_x_angle += 75;
+		}
+		return 1;
+	}
+	else if (current_value > (target + 75)){
+		ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+			*servo_x_angle -= 75;
+		}
+		return 1;
+	}
+	else{
+		ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+			*servo_x_angle = target;
+		}
+		return 0;
+	}
+	
+}
+
+void simultaneous_movement(){// to test maximum current draw
+	// move all servos to 180 degrees
+	while (move_by_step_until_target(&servo1_angle, 5000) | move_by_step_until_target(&servo2_angle, 5000) | move_by_step_until_target(&servo3_angle, 5000) | move_by_step_until_target(&servo4_angle, 5000) | move_by_step_until_target(&servo5_angle, 5000) | move_by_step_until_target(&servo6_angle, 5000))
+	{
+		_delay_ms(20);
+	}
+
+	_delay_ms(1000);
+
+	// move all servos to 0 degrees
+	while (move_by_step_until_target(&servo1_angle, 1000) | move_by_step_until_target(&servo2_angle, 1000) | move_by_step_until_target(&servo3_angle, 1000) | move_by_step_until_target(&servo4_angle, 1000) | move_by_step_until_target(&servo5_angle, 1000) | move_by_step_until_target(&servo6_angle, 1000))
+	{
+		_delay_ms(20);
+	}	
+	
+	//move all servos to 90 degrees
+	while (move_by_step_until_target(&servo1_angle, 3000) | move_by_step_until_target(&servo2_angle, 3000) | move_by_step_until_target(&servo3_angle, 3000) | move_by_step_until_target(&servo4_angle, 3000) | move_by_step_until_target(&servo5_angle, 3000) | move_by_step_until_target(&servo6_angle, 3000))
+	{
+		_delay_ms(20);
+	}	
+}
+
+int main(void) {
+	//position the robot to be in the 90 degrees rest position to prevent startup jerk
+	setup_ports();
+	setup_pwm();
+
+	servo_x_startup(&servo1_angle);
+		_delay_ms(1000);
+	servo_x_startup(&servo2_angle);
+		_delay_ms(1000);
+	servo_x_startup(&servo3_angle);
+		_delay_ms(1000);
+	servo_x_startup(&servo4_angle);
+		_delay_ms(1000);
+	servo_x_startup(&servo5_angle);
+		_delay_ms(1000);
+	servo_x_startup(&servo6_angle);
+		_delay_ms(1000);
+
+	simultaneous_movement();
+
+	while(1){
+		
+	}
+}
+
